f076d077ced64bf239c7951efdef830e0ce5464b
Massidama <massidama@outlook.it>
2023-11-20 21:00:26 +0100

    funzioni dei bottoni

--- js/script.js
+++ js/script.js
@@ -2,71 +2,70 @@
 // Selezione input dell'user
 const inputName = document.querySelector(".info-name");
 
-const inputKm = document.querySelector("info-km");
+const inputKm = document.querySelector(".info-km");
 
-const inputGender = document.querySelector("info-gender");
-
-
-// Selezione del bottone invio dati
-const sendButton = document.getElementById("genera-dati");
-
+const inputAge = document.querySelector(".info-age");
 
 // salviamo i dati input dell'user
 let infoName;
 let infoKm;
-let infoGender;
+let infoAge;
+
+// prezzo del biglietto dato da 0.21€ al km
+let priceTicket = 0.21 * inputKm.value;
+
+console.log(priceTicket);
+
+// Selezione del bottone invio dati
+const sendButton = document.getElementById("genera-dati");
 
 sendButton.addEventListener("click", 
 
     function () {
         infoName = inputName.value;
-        infoKm = inputKm.value;
-        infoGender = inputGender.value;
+        infoKm = parseInt(inputKm.value);
+        infoAge = parseInt(inputAge.value);
         
-        console.log(infoName, infoKm, infoGender);
+        document.querySelector(".name-user").innerHTML = inputName.value;
+
+        // condizioni per gli sconti che vanno effettuati
+        if (inputAge.value < 18) {
+            finalPrice = (priceTicket - (priceTicket * 20) / 100);
+            document.querySelector(".offerta").innerHTML = "Sconto under 18";
+        
+        } else if(inputAge.value > 65){
+            finalPrice = (priceTicket - (priceTicket * 40) / 100);
+            document.querySelector(".offerta").innerHTML = "Sconto over 65";
+        
+        } else {
+            finalPrice = priceTicket
+            document.querySelector(".offerta").innerHTML = "Biglietto standard";
+            
+        }      
+
+        finalPrice = finalPrice.toFixed(2)
+
+        document.getElementById("price-ticket").innerHTML = finalPrice;
+        document.querySelector(".carrozza-random").innerHTML = Math.floor(Math.random() * 100);
+        document.querySelector(".codice-random").innerHTML = Math.floor(Math.random() * 100000);
+
+        document.querySelector('.ghost').style.display='inline-block';
+    
     }
 
+);
+
+// selezione del bottone annulla
+const cancelButton = document.getElementById("resetta-dati");
+
+cancelButton.addEventListener("click",
+
+    function () {
+        document.querySelector('.ghost').style.display='none';
+    }
 );
 
 
 
 
 
-
-
-// // chiediamo all' utente il numero di km che vuole percorrere 
-// const kmUser = parseInt("Dichiarare i km che si vogliono percorrere");
-
-// // chiediamo all'utente la propria età
-// const ageUser = parseInt("Dichiarare la propria età");
-
-// // prezzo del biglietto dato da 0.21€ al km
-// let priceTicket = 0.21 * kmUser;
-
-// console.log(priceTicket);
-
-
-// // condizioni per gli sconti che vanno effettuati
-// if (ageUser < 18) {
-//     priceTicket = (priceTicket - (priceTicket * 20) / 100);
-
-//     console.log( "Il prezzo scontanto per persone sotto i 18 anni: " + priceTicket + " €" ) ;
- 
-// } else if(ageUser > 65){
-//     priceTicket = (priceTicket - (priceTicket * 40) / 100);
-
-//     console.log( "Il prezzo scontanto per persone sopra i 65 anni: " + priceTicket + " €" ) ;
- 
-// } else {
-
-//     console.log( "La tua età non rientra tra i prezzi scontati: " + priceTicket + " €" ) ;
- 
-// }
-
-// // funzione con massimo due decimali
-// priceTicket = priceTicket.toFixed(2)
-
-
-// // output del prezzo finale (con massimo due decimali)
-
-// document.getElementById("final-price").innerHTML = "Il prezzo del tuo biglietto è " + priceTicket + " €" ;
